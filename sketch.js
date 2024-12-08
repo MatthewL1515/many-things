b481becd69e1faab2330114d5d872f8054c8191f
Matthew Lee <Matthew_Lee@avenues-ny.org>
2024-12-07 22:35:33 -0500

    Adding Files

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let numCircles = 12 // Number of circles
+let circles = []
+let mouseCircle
+// important: search for noloop code
+
+function setup() {
+  // Create canvas
+  createCanvas(500, 500)
+  
+  // Function to start / restart the game
+  restartGame()
+}
+
+function draw() {
+  background(0);
+
+}
