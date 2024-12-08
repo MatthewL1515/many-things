231713ec5c49905b0623cb2e04bdcebee0bd6522
Matthew Lee <Matthew_Lee@avenues-ny.org>
2024-12-08 16:03:56 -0500

    All Circles Generated (but stationary)

--- sketch.js
+++ sketch.js
@@ -1,31 +1,57 @@
-let numCircles = 12 // Number of circles
-let circles = []
-let mouseCircle
-// important: search for noloop code
+let circles = [] // Array to store the 10 random circles
+let mouseCircle // Object for the mouse-controlled circle
+let numCircles = 10 // Number of random circles
 
 function setup() {
-  // Create canvas
-  createCanvas(500, 500)
-  
-  // Function to start / restart the game
-  restartGame()
+  createCanvas(400, 400)
+  startCircles()
 }
 
 function draw() {
-  background(0);
-  
+  background(0)
 
+  // Mouse Circle Position
+  mouseCircle.x = mouseX
+  mouseCircle.y = mouseY
+
+  // Circles
+  for (let circle of circles) {
+    drawCircle(circle) // Draw each random circle
+  }
+
+  // Mouse-controlled circle
+  drawCircle(mouseCircle)
 }
 
-function createCircle(size, color) {
+function createCircle(x, y, size, color) {
   return {
-    x: random(width),
-    y: random(height),
-    radius: radius,
-    dx: random (-3,3),
-    dy: random(-3,3),
+    x: x,
+    y: y,
+    r: size / 2,
     size: size,
-    r: size/2,
     color: color
   }
+}
+
+// Function to draw a circle
+function drawCircle(circle) {
+  fill(circle.color)
+  noStroke()
+  ellipse(circle.x, circle.y, circle.size)
+}
+
+function startCircles() {
+  circles = []
+  
+  // Create 10 random circles
+  for (let i = 0; i < numCircles; i++) {
+    let randomX = random(50, width - 50) // width - 50 to prevent wall spawn
+    let randomY = random(50, height - 50)
+    let randomSize = random(20, 50)
+    let randomColor = color(random(255), random(255), random(255))
+    circles.push(createCircle(randomX, randomY, randomSize, randomColor)) 
+    
+  }
+
+  mouseCircle = createCircle(mouseX, mouseY, 30, color(255, 0, 0))
 }
